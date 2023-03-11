fa875e4020560d5b0c2a61bfd62508777db2f5cc
Augasty <sayaksengupta140@gmail.com>
2023-03-11 22:09:33 +0530

    initial commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+
+
+function Home() {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+
+        </div>
+    )
+}
+
+export default Home
