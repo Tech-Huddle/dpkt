ddcb781c9e0c8363fb8059944dd813d4a6d41927
Augasty <sayaksengupta140@gmail.com>
2023-03-18 21:45:46 +0530

    header initial commit. -sayak

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import { Link, Route, Routes } from 'react-router-dom';
+import About from '../About/About';
+import Home from '../Home/Home';
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <div>Heading div</div>
+
+
+
+
+
+      <Navbar bg="dark" variant='dark' expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to='/'>DPKT</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to='/'>Home</Nav.Link>
+              <Nav.Link as={Link} to='/about'>About</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to = '/action1'>Action1</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to = '/action2'>Action2</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to = '/action3'>Action3</NavDropdown.Item>
+            </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+
+
+
+      {/* routes for the navbar component in header*/}
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        {/* to do: action tabs routes */}
+      </div>
+  );
 }
 
 export default Header
