1e4e7f74724bbfb54e490aca9db5003c038baa35
Augasty <sayaksengupta140@gmail.com>
2023-03-19 15:25:29 +0530

    separating the routes

---
+++ src/Components/Actions/Action1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Action1() {
+  return (
+    <div>Action1</div>
+  )
+}
---
+++ src/Components/Header/Routing.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Route, Routes } from 'react-router-dom';
+
+import About from '../About/About';
+import Home from '../Home/Home';
+import Action1 from '../Actions/Action1';
+import Action2 from '../Actions/Action2';
+import Action3 from '../Actions/Action3';
+
+
+export default function Routing() {
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/action1' element={<Action1/>}/>
+                <Route path='/action2' element={<Action2/>}/>
+                <Route path='/action3' element={<Action3/>}/>
+            
+            </Routes>
+        </div>
+    )
+}
