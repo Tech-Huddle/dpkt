b7def1f406620764ba195dd91aeec053ffef4207
chandan135 <chesschan135@gmail.com>
2023-04-09 19:14:33 +0530

    Demo

--- src/Components/Footer/Footer_part/Footer_part.js
+++ src/Components/Footer/Footer_part/Footer_part.js
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Footer_part.css";
 function Footer_part() {
-  return (
-    <div className="footer_footer">
-      <div className="content"></div>
-    </div>
-  );
+  return <div className="footer_footer"></div>;
 }
 
 export default Footer_part;
